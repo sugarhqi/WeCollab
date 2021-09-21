6da5fcc6266f6b85a9340bb5a747f30fc451c129
sugarhqi <hqi@sugarcrm.com>
2021-09-21 11:34:21 -0700

    Handle 'record:edit'

--- src/handlers/recordHandler.js
+++ src/handlers/recordHandler.js
@@ -1,17 +1,38 @@
+const _ = require('underscore');
+const records = {};
+
 module.exports = (io, socket) => {
-  const syncRecord = (payload) => {
-    // ...
-  }
+  const registerRecord = (record) => {
+    records[socket.id] = record;
+  };
 
-  const editRecord = (payload) => {
-    // ...
-  }
+  const unregisterRecord = () => {
+    delete records[socket.id];
+  };
 
-  const deleteRecord = (payload) => {
+  const syncRecord = (data) => {
     // ...
-  }
+  };
 
+  const editRecord = (data) => {
+    _.each(records, (record, socketId) => {
+        if (socketId !== socket.id &&
+          record.moduleName === data.moduleName &&
+          record.recordId === data.recordId) {
+            io.to(socketId).emit('record:edit', data);
+        }
+    });
+  };
+
+  const deleteRecord = (data) => {
+    // ...
+  };
+
+  socket.on("record:register", registerRecord);
+  socket.on("record:unregister", unregisterRecord);
+  socket.on("disconnect", unregisterRecord);
   socket.on("record:sync", syncRecord);
   socket.on("record:edit", editRecord);
   socket.on("record:delete", deleteRecord);
 }
+
